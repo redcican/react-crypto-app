89703d13ca03ca8045350577a9e0a3e5ce995909
shikun chen <shikun.chen@stud.uni-due.de>
2022-03-23 23:03:20 +0100

    init

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+}
+
+const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders});
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query(
+            {
+                query: (count) => createRequest(`/coins?limit=${count}`),
+            }),
+        getCryptoDetails: builder.query(
+            {
+                query: (coinId) => createRequest(`/coin/${coinId}`),
+            }
+        ),
+        getCryptoHistory: builder.query(
+            {
+                query: ({ coinId, timePeriod }) => createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+            }
+        ),
+        getExchanges: builder.query(
+            {
+                query: () => createRequest(`/exchanges`),
+            }
+        ),
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
+} = cryptoApi;
